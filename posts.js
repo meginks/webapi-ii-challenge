7cfcdcb8b3f01e0e392afe5f6cf1252e24aed6a3
Megan Jones <38929577+meginks@users.noreply.github.com>
2019-03-26 15:35:45 -0500

    get request working

--- posts.js
+++ posts.js
@@ -1,8 +1,31 @@
 const express = require('express'); 
-
+const db = require('./data/db.js'); 
 const router = express.Router(); 
 
 
+// GET 
+
+router.get('/', async (req, res) => {
+    try {
+        const posts = await db.find(req.query); 
+        res.status(200).json(posts) 
+    } catch (error) {
+        console.log("GET error", error);
+        res.status(500)
+        .json({
+           message: `The post information could not be received: ${error}` 
+        })
+    }
+})
+
+// POST 
+
+
+// PUT 
+
+
+
+// DELETE 
 
 
 
