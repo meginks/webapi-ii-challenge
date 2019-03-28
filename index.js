11a3d28d9d4b368464c73921afa83b1eab90e631
Megan Jones <38929577+meginks@users.noreply.github.com>
2019-03-28 15:08:43 -0500

    changed the port const

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ require('dotenv').config();
 const server = require('./server'); 
 
 let port = process.env.PORT; 
-if (port = null || port == "") {
-    port = 8000;
-}
+
 
 server.listen(port, () => {
     console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
